055d4555cc9a5b26ec47a83294f41bc29fdc9cb3
Ramkumar-Anandan <ramkumar.anandan.be@gmail.com>
2021-03-16 16:09:21 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,75 @@
 const boxElement=document.querySelectorAll(".box");
+var winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+var xAttempts = [];
+var oAttempts = [];
+var click=0;
+var wonTheGame = 0;
+const message= document.getElementById("message");
+const gameResult=document.getElementById("result");
+const restart=document.getElementById("button");
 
-const class1="text";
-
-var i=0;
 boxElement.forEach(box=>{
-    console.log(box)
-    box.addEventListener('click',handleClick,{once:true})
+    console.log(box);
+    box.onclick  = handleClick;
 })
-
-
-
 function handleClick(e){
-    console.log(e.target.getAttribute('id'))
-    i=e.target.getAttribute('id');
-    console.log("working");
-    boxElement[i-1].classList.add(class1);
-    boxElement[i-1].innerHTML="X";
+    console.log(e.target.getAttribute('id'));
+    const i=e.target.getAttribute('id');
+    const text = document.createElement('p');
+    text.setAttribute('id','text');
+    boxElement[i-1].appendChild(text);
+    console.log(boxElement[i-1]);
+    if(click%2 == 0){
+        xAttempts.push(parseInt(i-1));
+        console.log(xAttempts);
+        text.innerHTML="X";
+        text.style.color = '#FAB201';
+        result(winningCombinations,xAttempts,"X");
+    }
+    else{
+        oAttempts.push(parseInt(i-1));
+        console.log(oAttempts)
+        text.innerHTML="O";
+        text.style.color = '#FAB201';
+        result(winningCombinations,oAttempts,"O");
+    }
+    click++;
+    if(click == 9 && wonTheGame == 0){ 
+            gameResult.style.visibility="visible";
+            message.innerHTML = "It's a tie 🤝 ";
+    }
+}
+function result(winningCombinations, attempts, player){
+    let flag = 0;
+    let checker = [];
+    for (var i = 0; i < winningCombinations.length; i++) {
+        if (Array.isArray(winningCombinations[i])){
+            result(winningCombinations[i],attempts,player);
+        }else{
+            if(attempts.includes(winningCombinations[i])){
+                checker.push(true);
+                flag++;
+            } else {
+                checker.push(false);
+            }
+        }
+    }
+    if (checker.every(check => check === true)&&flag>2){
+            gameResult.style.visibility="visible";
+            message.innerHTML ="'"+ player +"'" + " Won the game!";  
+            wonTheGame=1;   
+    }
 }
 
-// for(var i = 0; i < boxElement.length; i++){
-//     var num = Math.floor(Math.random() * tileNumbers.lenth);
-//     boxElement.classList.add(class1);
-//     //tileNumbers.pop(num);
-// }
+restart.onclick=()=>{
+    location.reload();
+}
